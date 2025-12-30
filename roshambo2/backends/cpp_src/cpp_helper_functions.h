8fd90e0e8992fe56ae56f181fc56e3ae86ed883e
dave <davidacosgroveaz@gmail.com>
2025-12-30 14:20:27 +0000

    Move function docstring to include.

--- roshambo2/backends/cpp_src/cpp_helper_functions.h
+++ roshambo2/backends/cpp_src/cpp_helper_functions.h
@@ -17,8 +17,8 @@ using DTYPE = float;
 /// @param arr
 /// @param scalar
 /// @return
-template<typename T, size_t N>
-std::array<T, N> operator/(const std::array<T, N>& arr, const T& scalar);
+template <typename T, size_t N>
+std::array<T, N> operator/(const std::array<T, N> &arr, const T &scalar);
 
 /// @brief product of std::array by scalar
 /// @tparam T
@@ -26,8 +26,8 @@ std::array<T, N> operator/(const std::array<T, N>& arr, const T& scalar);
 /// @param arr
 /// @param scalar
 /// @return
-template<typename T, size_t N>
-std::array<T, N> operator*(const T& scalar, const std::array<T, N>& arr);
+template <typename T, size_t N>
+std::array<T, N> operator*(const T &scalar, const std::array<T, N> &arr);
 
 /// @brief addition of std::arrays
 /// @tparam T
@@ -35,8 +35,9 @@ std::array<T, N> operator*(const T& scalar, const std::array<T, N>& arr);
 /// @param arr1
 /// @param arr2
 /// @return
-template<typename T, size_t N>
-std::array<T, N> operator+(const std::array<T, N>& arr1, const std::array<T, N>& arr2);
+template <typename T, size_t N>
+std::array<T, N> operator+(const std::array<T, N> &arr1,
+                           const std::array<T, N> &arr2);
 
 /// @brief product of std::arrays
 /// @tparam T
@@ -44,45 +45,48 @@ std::array<T, N> operator+(const std::array<T, N>& arr1, const std::array<T, N>&
 /// @param arr1
 /// @param arr2
 /// @return
-template<typename T, size_t N>
-std::array<T, N> operator*(const std::array<T, N>& arr1, const std::array<T, N>& arr2);
+template <typename T, size_t N>
+std::array<T, N> operator*(const std::array<T, N> &arr1,
+                           const std::array<T, N> &arr2);
 
 /// @brief 3x3 matrix x vector
 /// @param mat matrix[3,3]
 /// @param vec 3 vector[3]
 /// @param result 3 vector[3]
-void matvec3x3x3(DTYPE mat[][3], const DTYPE * vec, DTYPE * result);
+void matvec3x3x3(DTYPE mat[][3], const DTYPE *vec, DTYPE *result);
 
 /// @brief 3x3 matrix x vector
 /// @param mat matrix[3,3]
 /// @param vec 3 vector[3]
 /// @return result 3 vector[3]
-std::array<DTYPE,3> matvec3x3x3(const std::array<std::array<DTYPE,3>,3> &mat, const DTYPE * vec);
+std::array<DTYPE, 3> matvec3x3x3(const std::array<std::array<DTYPE, 3>, 3> &mat,
+                                 const DTYPE *vec);
 
 /// @brief 3x3 matrix x vector
 /// @param mat matrix[3,3]
 /// @param vec 3 vector[3]
 /// @param result 3 vector[3]
-void matvec3x3x3(const std::array<std::array<DTYPE,3>,3> &mat, const DTYPE * vec, DTYPE * result);
+void matvec3x3x3(const std::array<std::array<DTYPE, 3>, 3> &mat,
+                 const DTYPE *vec, DTYPE *result);
 
 /// @brief convert quaternion to rotation matrix
 /// @param q quaternion[4]
 /// @param M matrix[3,3]
-void quaternion_to_rotation_matrix(std::array<DTYPE,4> &q, DTYPE M[3][3]);
+void quaternion_to_rotation_matrix(std::array<DTYPE, 4> &q, DTYPE M[3][3]);
 
 /// @brief transform molB by mat
 /// @param mat matrix[3,3]
 /// @param molB molecular coordinates[N,3]
 /// @param molBT molecular coordinates[N,3]
 /// @param NmolB N
-void transform(DTYPE mat[][3], const DTYPE * molB, DTYPE * molBT, int NmolB);
+void transform(DTYPE mat[][3], const DTYPE *molB, DTYPE *molBT, int NmolB);
 
 /// @brief translate molA to molAT by t
 /// @param t vector[3]
 /// @param molA molecular coordinates[N,3]
 /// @param molAT molecular coordinates[N,3]
 /// @param NmolA N
-void translate(DTYPE t[3], const DTYPE * molA, DTYPE * molAT, int NmolA);
+void translate(DTYPE t[3], const DTYPE *molA, DTYPE *molAT, int NmolA);
 
 ///////////////////////////////////////////////////////////////////////////////
 /// Volume functions
@@ -94,7 +98,7 @@ void translate(DTYPE t[3], const DTYPE * molA, DTYPE * molAT, int NmolA);
 /// @param molB
 /// @param NmolB
 /// @return volume
-DTYPE volume(const DTYPE * molA, int NmolA, const DTYPE * molB, int NmolB);
+DTYPE volume(const DTYPE *molA, int NmolA, const DTYPE *molB, int NmolB);
 
 /// @brief color overlap volume of molA and molB
 /// @param molA
@@ -107,9 +111,9 @@ DTYPE volume(const DTYPE * molA, int NmolA, const DTYPE * molB, int NmolB);
 /// @param pmat
 /// @param N_features
 /// @return volume
-DTYPE volume_color(const DTYPE * molA, int NmolA, const int * molA_type,
-                   const DTYPE * molB, int NmolB, const int * molB_type,
-                   const DTYPE * rmat, const DTYPE * pmat, int N_features);
+DTYPE volume_color(const DTYPE *molA, int NmolA, const int *molA_type,
+                   const DTYPE *molB, int NmolB, const int *molB_type,
+                   const DTYPE *rmat, const DTYPE *pmat, int N_features);
 
 ////////////////////////////////////////////////////////////////////////////////
 /// gradient functions
@@ -120,8 +124,10 @@ DTYPE volume_color(const DTYPE * molA, int NmolA, const int * molA_type,
 /// @param NmolA
 /// @param molB
 /// @param NmolB
-/// @return array[7] containing the quaternion gradients and the position gradients
-std::array<DTYPE,7> get_gradient(const DTYPE * molA, int NmolA, const DTYPE * molB, int NmolB);
+/// @return array[7] containing the quaternion gradients and the position
+/// gradients
+std::array<DTYPE, 7> get_gradient(const DTYPE *molA, int NmolA,
+                                  const DTYPE *molB, int NmolB);
 
 /// @brief compute overlap color gradients of molA and molB w.r.t molB
 /// @param molA
@@ -133,27 +139,60 @@ std::array<DTYPE,7> get_gradient(const DTYPE * molA, int NmolA, const DTYPE * mo
 /// @param rmat
 /// @param pmat
 /// @param N_features
-/// @return array[7] containing the quaternion gradients and the position gradients
-std::array<DTYPE,7> get_gradient_color(const DTYPE * molA, int NmolA, const int * molA_type,
-                                       const DTYPE * molB, int NmolB, const int * molB_type,
-                                       const DTYPE * rmat, const DTYPE * pmat, int N_features);
+/// @return array[7] containing the quaternion gradients and the position
+/// gradients
+std::array<DTYPE, 7> get_gradient_color(const DTYPE *molA, int NmolA,
+                                        const int *molA_type, const DTYPE *molB,
+                                        int NmolB, const int *molB_type,
+                                        const DTYPE *rmat, const DTYPE *pmat,
+                                        int N_features);
 
 ////////////////////////////////////////////////////////////////////////////////
 /// Optimization functions
 ////////////////////////////////////////////////////////////////////////////////
 
-
 // Adagrad optimization step
-void adagrad_step(std::array<DTYPE,4> &q, std::array<DTYPE,3> &t,  std::array<DTYPE,7> g,
-                  std::array<DTYPE,7> &cache, DTYPE lr_q, DTYPE lr_t);
+void adagrad_step(std::array<DTYPE, 4> &q, std::array<DTYPE, 3> &t,
+                  std::array<DTYPE, 7> g, std::array<DTYPE, 7> &cache,
+                  DTYPE lr_q, DTYPE lr_t);
 
-void single_conformer_optimiser(const DTYPE *molA, DTYPE *molAT, const int *molA_type,
-                                int NmolA, int NmolA_real, int NmolA_color,
-                                DTYPE self_overlap_A, DTYPE self_overlap_A_color,
-                                const DTYPE *molB, DTYPE *molBT, const int *molB_type,
-                                int NmolB, int NmolB_real, int NmolB_color,
-                                const DTYPE *rmat, const DTYPE *pmat, int N_features,
-                                bool optim_color, DTYPE mixing_param, DTYPE lr_q, DTYPE lr_t, int nsteps,
-                                DTYPE *scores);
+/// @brief Do the overlay for a single conformer of A against a single conformer
+/// of B
+/// @param molA - the query molecule as 1D array of 4 * NmolA entries. Each
+/// block of 4 is the coords and w parameter
+/// @param molAT - the working copy of A
+/// @param molA_type - the features types for molecule A
+/// @param NmolA - the number of atoms and features in A
+/// @param NmolA_real - the number of atoms in A
+/// @param NmolA_color - the number of features in A
+/// @param self_overlap_A - overlap volume of A with itself
+/// @param self_overlap_A_color - color overlap of A with itself
+/// @param molB - the target molecule as 1D array of 4 * NmolB entries. Each
+/// block of 4 is the coords and w parameter
+/// @param molBT - the working copy of B
+/// @param molB_type - the features types for molecule B
+/// @param NmolB - the number of atoms and features in B
+/// @param NmolB_real - the number of atoms in B
+/// @param NmolB_color - the number of features in B
+/// @param rmat - interaction matrix r - linearised square matrix for looking up
+/// r for features
+/// @param pmat - interaction matrix p - linearised square matrix for looking up
+/// p for features
+/// @param N_features - the number of feature types for looking up values in
+/// rmat and pmat
+/// @param optim_color - whether to optimise on colors as well as shape
+/// @param mixing_param - how to mix the 2 tanimoto values
+/// @param lr_q - scale factor for optimising the quaternion (?)
+/// @param lr_t - scale factor for optimising the translation (?)
+/// @param nsteps - number of optimiser steps
+/// @params scores - the output scores and transformation to reproduce the
+/// overlay - an array of size 20
+void single_conformer_optimiser(
+    const DTYPE *molA, DTYPE *molAT, const int *molA_type, int NmolA,
+    int NmolA_real, int NmolA_color, DTYPE self_overlap_A,
+    DTYPE self_overlap_A_color, const DTYPE *molB, DTYPE *molBT,
+    const int *molB_type, int NmolB, int NmolB_real, int NmolB_color,
+    const DTYPE *rmat, const DTYPE *pmat, int N_features, bool optim_color,
+    DTYPE mixing_param, DTYPE lr_q, DTYPE lr_t, int nsteps, DTYPE *scores);
 
-#endif //_ROSHAMBO2_CPP_HELPER_FUNCTIONS_H
+#endif  //_ROSHAMBO2_CPP_HELPER_FUNCTIONS_H
